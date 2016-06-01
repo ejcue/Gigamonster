432d6eb25fd76c1d32e66e6e3550d7269a672ab1
Eric Cuevas <ejcue@me.com>
2016-05-31 22:50:23 -0500

    added sfx, revive button, and made penalties effected by food.

--- Gigamonster/ViewController.swift
+++ Gigamonster/ViewController.swift
@@ -7,32 +7,169 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var monsterImg: MonsterImg!
     
     @IBOutlet weak var foodImg: DragImg!
     
     @IBOutlet weak var heartImg: DragImg!
     
+    @IBOutlet weak var penalty1: UIImageView!
+    
+    @IBOutlet weak var penalty2: UIImageView!
+    
+    @IBOutlet weak var penalty3: UIImageView!
+    
+    @IBOutlet weak var reviveBtn: UIButton!
+    
+    @IBAction func revive(sender: AnyObject) {
+        penalties = 0
+        penalty1.alpha = DIM_ALPHA
+        penalty2.alpha = DIM_ALPHA
+        penalty3.alpha = DIM_ALPHA
+        
+        reviveBtn.hidden = true
+        monsterImg.playIdleAnimation()
+        startTimer()
+        
+    }
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
+    var monsterHappy = false
+    var currentItem: UInt32 = 0
+    
+    var musicPLayer: AVAudioPlayer!
+    var sfxBite: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
         
-        var imgArray = [UIImage]()
-        for var x = 1; x <= 4; x++ {
-            let img = UIImage(named: "idle\(x).png")
-            imgArray.append(img!)
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        reviveBtn.hidden = true
+        
+        penalty1.alpha = DIM_ALPHA
+        penalty2.alpha = DIM_ALPHA
+        penalty3.alpha = DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+        
+        do {
+            try musicPLayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cave-music", ofType:"mp3")!))
+            
+            try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType:"wav")!))
+            
+            try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType:"wav")!))
+            
+            try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType:"wav")!))
+            
+            try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType:"wav")!))
+            musicPLayer.prepareToPlay()
+            musicPLayer.play()
+            
+            sfxBite.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            sfxSkull.prepareToPlay()
+            
+        }catch let err as NSError {
+            print(err.debugDescription)
         }
         
-        monsterImg.animationImages = imgArray
-        monsterImg.animationDuration = 0.8
-        monsterImg.animationRepeatCount = 0
-        monsterImg.startAnimating()
+        startTimer()
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        monsterHappy = true
+        startTimer()
+        
+        foodImg.alpha = DIM_ALPHA
+        foodImg.userInteractionEnabled = false
+        
+        heartImg.alpha = DIM_ALPHA
+        heartImg.userInteractionEnabled = false
+        
+        if currentItem == 0 {
+            sfxHeart.play()
+        } else {
+            sfxBite.play()
+        }
     }
 
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+    
+    timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        
+        if !monsterHappy {
+            penalties++
+            sfxSkull.play()
+            
+            if penalties == 1 {
+                penalty1.alpha = OPAQUE
+                penalty2.alpha = DIM_ALPHA
+                
+            } else if penalties == 2 {
+                penalty2.alpha = OPAQUE
+                penalty3.alpha = DIM_ALPHA
+            }else if penalties >= 3{
+                penalty3.alpha = OPAQUE
+            }else {
+                penalty1.alpha = DIM_ALPHA
+                penalty2.alpha = DIM_ALPHA
+                penalty3.alpha = DIM_ALPHA
+            }
+            
+            if penalties >= MAX_PENALTIES {
+                gameOver()         
+            }
+        }
+        
+        let rand = arc4random_uniform(2)
+        
+        if rand == 0 {
+            foodImg.alpha = DIM_ALPHA
+            foodImg.userInteractionEnabled = false
+            
+            heartImg.alpha = OPAQUE
+            heartImg.userInteractionEnabled = true
+        } else {
+            heartImg.alpha = DIM_ALPHA
+            heartImg.userInteractionEnabled = false
+            
+            foodImg.alpha = OPAQUE
+            foodImg.userInteractionEnabled = true
+        }
+       
+        currentItem = rand
+        monsterHappy = false
+    }
+    
+    func gameOver() {
+     timer.invalidate()
+     monsterImg.playDeathAnimation()
+        sfxDeath.play()
+        reviveBtn.hidden = false
+        
+    }
     
     
 
